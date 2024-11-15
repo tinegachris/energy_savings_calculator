29a1800b17f060466b06879f1f0479d95bd95c37
tingachris <chrispinetinega@gmail.com>
2024-11-15 15:21:04 +0300

    Refactor documentation for cost savings from genset efficiency improvements

--- scripts/genset_fuel_savings.py
+++ scripts/genset_fuel_savings.py
@@ -1,8 +1,9 @@
 import json
 import csv
 from pathlib import Path
+import os
 import sys
-from typing import Dict, List, Tuple
+from typing import Dict, List, Tuple, Optional
 import xlsxwriter
 import logging
 
@@ -16,8 +17,9 @@ class CalculateGensetSavings:
       "January", "February", "March", "April", "May", "June",
       "July", "August", "September", "October", "November", "December"
     ]
-    self.solar_yield_data = self.load_solar_yield_data()
-    self.genset_grid_yield_data = self.load_genset_grid_yield_data()
+    self.solar_yield_data = self.load_yield_data("Solar-Energy-Yield-Month.csv", "Solar Energy Yield Month")
+    self.grid_yield_data = self.load_yield_data("Grid-Energy-Yield-Month.csv", "Grid Energy Yield Month")
+    self.genset_yield_data = self.load_yield_data("Genset-Energy-Yield-Month.csv", "Genset Energy Yield Month")
 
   def load_config(self) -> Dict:
     """Load configuration from a JSON file."""
@@ -33,20 +35,20 @@ class CalculateGensetSavings:
 
   def confirm_year_folder(self) -> None:
     """Confirm the existence of the year folder."""
-    year_folder = Path(str(self.config["year"]))
+    year_folder = Path(f"data/genset_savings_data/{self.config['year']}")
     if not year_folder.exists():
-      logging.error(f"Year {self.config['year']} folder does not exist. Exiting.")
+      logging.error(f"data/genset_savings_data/{self.config['year']} folder does not exist. Exiting.")
       sys.exit()
-    logging.info(f"Year {self.config['year']} folder exists.")
+    logging.info(f"data/genset_savings_data/{self.config['year']} folder exists.")
 
   def clean_month_csv_files(self) -> None:
     """Remove the 'Conditions Met' column, remove previously calculated savings, and remove empty rows from the CSV file."""
     refined_data = {}
     for month in self.months_list:
       refined_month_data = []
-      file_path = Path(f"{self.config['year']}/{month}-Genset-Savings.csv")
+      file_path = Path(f"data/genset_savings_data/{self.config['year']}/{month}-Genset-Savings.csv")
       if not file_path.exists():
-        logging.warning(f"{month}-Genset-Savings.csv does not exist.")
+        logging.warning(f"data/genset_savings_data/{month}-Genset-Savings.csv does not exist.")
         continue
       with open(file_path, mode='r', newline='') as infile:
         reader = csv.DictReader(infile)
@@ -58,7 +60,7 @@ class CalculateGensetSavings:
       refined_data[month] = refined_month_data
 
     for month, data in refined_data.items():
-      file_path = Path(f"{self.config['year']}/{month}-Genset-Savings.csv")
+      file_path = Path(f"data/genset_savings_data/{self.config['year']}/{month}-Genset-Savings.csv")
       with open(file_path, mode='w', newline='') as outfile:
         writer = csv.DictWriter(outfile, fieldnames=fieldnames)
         writer.writeheader()
@@ -68,7 +70,7 @@ class CalculateGensetSavings:
   def remove_short_time_entries(self) -> None:
     """Remove rows with 'Time Elapsed (minutes)' less than 1 from the CSV files."""
     for month in self.months_list:
-      file_path = Path(f"{self.config['year']}/{month}-Genset-Savings.csv")
+      file_path = Path(f"data/genset_savings_data/{self.config['year']}/{month}-Genset-Savings.csv")
       if not file_path.exists():
         logging.warning(f"{month}-Genset-Savings.csv does not exist.")
         continue
@@ -85,51 +87,37 @@ class CalculateGensetSavings:
         writer.writerows(filtered_data)
       logging.info(f"Removed rows with 'Time Elapsed (minutes)' < 1 from {file_path}.")
 
-  def load_solar_yield_data(self) -> Dict[str, str]:
-    """Load solar yield data from CSV file."""
+  def load_yield_data(self, filename: str, yield_column: str) -> Dict[str, str]:
+    """Load yield data from CSV file."""
     year = self.config["year"]
-    solar_yield_file = Path(f"{year}/Solar-Energy-Yield-Month.csv")
-    if not solar_yield_file.exists():
-      logging.error(f"Solar-Energy-Yield-Month.csv does not exist. Exiting.")
-      sys.exit()
-    with open(solar_yield_file, mode='r', newline='') as infile:
+    yield_file = Path(f"data/yield_data{year}/{filename}")
+    if not yield_file.exists():
+      logging.warning(f"{filename} does not exist. Skipping.")
+      return {}
+    with open(yield_file, mode='r', newline='') as infile:
       reader = csv.DictReader(infile)
       fieldnames = [field.strip() for field in reader.fieldnames]
-      if 'Category' not in fieldnames or 'Solar Energy Yield Month' not in fieldnames:
-        logging.error(f"Required columns not found in {solar_yield_file}. Exiting.")
-        sys.exit()
-      solar_yield_data = {row["Category"]: row["Solar Energy Yield Month"] for row in reader}
-    return solar_yield_data
-
-  def load_genset_grid_yield_data(self) -> Dict[str, Tuple[str, str]]:
-    """Load genset and grid yield data from CSV file."""
-    year = self.config["year"]
-    genset_grid_yield_file = Path(f"{year}/Genset-and-Grid-Yield.csv")
-    if not genset_grid_yield_file.exists():
-      logging.error(f"Genset-and-Grid-Yield.csv does not exist. Exiting.")
-      sys.exit()
-    with open(genset_grid_yield_file, mode='r', newline='') as infile:
-      reader = csv.DictReader(infile)
-      fieldnames = [field.strip() for field in reader.fieldnames]
-      if 'Category' not in fieldnames or 'Grid Energy Yield Month' not in fieldnames or 'Genset Energy Yield Month' not in fieldnames:
-        logging.error(f"Required columns not found in {genset_grid_yield_file}. Exiting.")
-        sys.exit()
-      genset_grid_yield_data = {row["Category"]: (row["Grid Energy Yield Month"], row["Genset Energy Yield Month"]) for row in reader}
-    return genset_grid_yield_data
+      if 'Category' not in fieldnames or yield_column not in fieldnames:
+        logging.error(f"Required columns not found in {yield_file}. Skipping.")
+        return {}
+      yield_data = {row["Category"]: row[yield_column] for row in reader}
+    return yield_data
 
   def calculate_genset_savings(self) -> None:
     """Copy CSV data of each month into the XLSX file and calculate savings."""
     year = self.config["year"]
     site = self.config["site"]
-    file_name = f"{year}_{site}_Genset_Fuel_Savings.xlsx"
+    results_dir = Path('results')
+    results_dir.mkdir(exist_ok=True)
+    file_name = results_dir / f"{year}_{site}_Genset_Fuel_Savings.xlsx"
     workbook = xlsxwriter.Workbook(file_name)
     logging.info(f"Created XLSX file: {file_name}")
 
     for month in self.months_list:
       worksheet = workbook.add_worksheet(f"{site}_{month}_Savings")
-      file_path = Path(f"{year}/{month}-Genset-Savings.csv")
+      file_path = Path(f"data/genset_savings_data/{year}/{month}-Genset-Savings.csv")
       if not file_path.exists():
-        logging.warning(f"{month}-Genset-Savings.csv does not exist. Skipping.")
+        logging.warning(f"{file_path} does not exist. Skipping.")
         continue
       with open(file_path, mode='r', newline='') as infile:
         reader = list(csv.reader(infile))
@@ -146,7 +134,7 @@ class CalculateGensetSavings:
     workbook.close()
     logging.info(f"Completed calculating genset savings and writing: {file_name}")
 
-  def _write_calculations_to_worksheet(self, worksheet, reader: List[List[str]], month: str) -> None:
+  def _write_calculations_to_worksheet(self, worksheet: xlsxwriter.Workbook.worksheet_class, reader: List[List[str]], month: str) -> None:
     """Write calculations to the worksheet."""
     energy_saving_col_idx = None
     for col_idx, col_name in enumerate(reader[0]):
@@ -162,6 +150,7 @@ class CalculateGensetSavings:
     genset_fuel_savings = total_kwh_saved * cost_fuel_plus_MTCE
     cost_per_outage = self.config["cost_per_outage"]
     outage_savings = num_outages * cost_per_outage
+    total_month_savings = genset_fuel_savings + outage_savings
     worksheet.write(len(reader) + 2, 0, "Total kWh Saved")
     worksheet.write(len(reader) + 2, 1, total_kwh_saved)
     worksheet.write(len(reader) + 4, 0, "Number of Outages")
@@ -173,17 +162,11 @@ class CalculateGensetSavings:
     worksheet.write(len(reader) + 10, 0, "Solar Yield")
     worksheet.write(len(reader) + 10, 1, self.solar_yield_data.get(month, "N/A"))
     worksheet.write(len(reader) + 11, 0, "Grid Yield")
-    worksheet.write(len(reader) + 11, 1, self.genset_grid_yield_data.get(month, ("N/A", "N/A"))[0])
+    worksheet.write(len(reader) + 11, 1, self.grid_yield_data.get(month, "N/A"))
     worksheet.write(len(reader) + 12, 0, "Genset Yield")
-    worksheet.write(len(reader) + 12, 1, self.genset_grid_yield_data.get(month, ("N/A", "N/A"))[1])
-    worksheet.write(len(reader) + 14, 0, "Power Quality Savings")
-    worksheet.write(len(reader) + 14, 1, "N/A")
-    worksheet.write(len(reader) + 16, 0, "Energy Efficiency Savings")
-    worksheet.write(len(reader) + 16, 1, "N/A")
-    worksheet.write(len(reader) + 18, 0, "Maintenance Savings")
-    worksheet.write(len(reader) + 18, 1, "N/A")
-    worksheet.write(len(reader) + 20, 0, "Total Savings")
-    worksheet.write(len(reader) + 20, 1, "N/A")
+    worksheet.write(len(reader) + 12, 1, self.genset_yield_data.get(month, "N/A"))
+    worksheet.write(len(reader) + 20, 0, "Total Month Savings")
+    worksheet.write(len(reader) + 20, 1, total_month_savings)
     for col_idx, col_name in enumerate(reader[0]):
       max_len = max(len(str(cell)) for cell in [col_name] + [row[col_idx] for row in reader[1:]])
       worksheet.set_column(col_idx, col_idx, max_len + 2)
@@ -194,7 +177,6 @@ class CalculateGensetSavings:
     year = self.config["year"]
     site = self.config["site"]
     summary_worksheet = workbook.add_worksheet(f"{site}_{year}_Savings_Summary")
-
     summary_worksheet.write(0, 0, "Month")
     summary_worksheet.write(0, 1, "Total kWh Saved")
     summary_worksheet.write(0, 2, "Number of Outages")
@@ -203,10 +185,7 @@ class CalculateGensetSavings:
     summary_worksheet.write(0, 5, "Solar Yield")
     summary_worksheet.write(0, 6, "Grid Yield")
     summary_worksheet.write(0, 7, "Genset Yield")
-    summary_worksheet.write(0, 8, "Power Quality Savings")
-    summary_worksheet.write(0, 9, "Energy Efficiency Savings")
-    summary_worksheet.write(0, 10, "Maintenance Savings")
-    summary_worksheet.write(0, 11, "Total Savings")
+    summary_worksheet.write(0, 8, "Total Month Savings")
 
     total_kwh_saved_year = 0
     total_num_outages_year = 0
@@ -215,62 +194,51 @@ class CalculateGensetSavings:
     total_solar_yield_year = 0
     total_grid_yield_year = 0
     total_genset_yield_year = 0
-    total_power_quality_savings_year = 0
-    total_energy_efficiency_savings_year = 0
-    total_maintenance_savings_year = 0
     total_savings_year = 0
 
     for row_idx, month in enumerate(self.months_list, start=1):
-      file_path = Path(f"{year}/{month}-Genset-Savings.csv")
-      if not file_path.exists():
-        logging.warning(f"{month}-Genset-Savings.csv does not exist. Skipping.")
+      worksheet = workbook.get_worksheet_by_name(f"{site}_{month}_Savings")
+      if worksheet is None:
         continue
-      with open(file_path, mode='r', newline='') as infile:
-        reader = list(csv.reader(infile))
-        energy_saving_col_idx = None
-        for col_idx, col_name in enumerate(reader[0]):
-          if col_name == "Energy Saving (kWh)":
-            energy_saving_col_idx = col_idx
-            break
-        if energy_saving_col_idx is None:
-          logging.warning(f"Energy Saving (kWh) column not found in {month}-Genset-Savings.csv. Skipping calculations.")
-          continue
-        total_kwh_saved = sum(float(row[energy_saving_col_idx]) for row in reader[1:] if row[energy_saving_col_idx])
-        num_outages = len([row for row in reader[1:] if row[energy_saving_col_idx]]) - 1
-        cost_fuel_plus_MTCE = self.config["cost_fuel_plus_MTCE"]
-        genset_fuel_savings = total_kwh_saved * cost_fuel_plus_MTCE
-        cost_per_outage = self.config["cost_per_outage"]
-        outage_savings = num_outages * cost_per_outage
-        total_savings = genset_fuel_savings + outage_savings
-        summary_worksheet.write(row_idx, 11, total_savings)
-        total_savings_year += total_savings
 
-        summary_worksheet.write(row_idx, 0, month)
-        summary_worksheet.write(row_idx, 1, total_kwh_saved)
-        summary_worksheet.write(row_idx, 2, num_outages)
-        summary_worksheet.write(row_idx, 3, genset_fuel_savings)
-        summary_worksheet.write(row_idx, 4, outage_savings)
-        summary_worksheet.write(row_idx, 5, self.solar_yield_data.get(month, "N/A"))
-        summary_worksheet.write(row_idx, 6, self.genset_grid_yield_data.get(month, ("N/A", "N/A"))[0])
-        summary_worksheet.write(row_idx, 7, self.genset_grid_yield_data.get(month, ("N/A", "N/A"))[1])
-        summary_worksheet.write(row_idx, 8, "N/A")
-        summary_worksheet.write(row_idx, 9, "N/A")
-        summary_worksheet.write(row_idx, 10, "N/A")
-        summary_worksheet.write(row_idx, 11, "N/A")
+      num_rows = worksheet.dim_rowmax + 1
+      total_kwh_saved = worksheet.get_cell_value(num_rows + 2, 1)
+      num_outages = worksheet.get_cell_value(num_rows + 4, 1)
+      genset_fuel_savings = worksheet.get_cell_value(num_rows + 6, 1)
+      outage_savings = worksheet.get_cell_value(num_rows + 8, 1)
+      solar_yield = worksheet.get_cell_value(num_rows + 10, 1)
+      grid_yield = worksheet.get_cell_value(num_rows + 11, 1)
+      genset_yield = worksheet.get_cell_value(num_rows + 12, 1)
+      total_month_savings = worksheet.get_cell_value(num_rows + 20, 1)
 
-        total_kwh_saved_year += total_kwh_saved
-        total_num_outages_year += num_outages
-        total_genset_fuel_savings_year += genset_fuel_savings
-        total_outage_savings_year += outage_savings
-        total_solar_yield_year += float(self.solar_yield_data.get(month, 0))
-        total_grid_yield_year += float(self.genset_grid_yield_data.get(month, ("0", "0"))[0])
-        total_genset_yield_year += float(self.genset_grid_yield_data.get(month, ("0", "0"))[1])
+      summary_worksheet.write(row_idx, 0, month)
+      summary_worksheet.write(row_idx, 1, total_kwh_saved)
+      summary_worksheet.write(row_idx, 2, num_outages)
+      summary_worksheet.write(row_idx, 3, genset_fuel_savings)
+      summary_worksheet.write(row_idx, 4, outage_savings)
+      summary_worksheet.write(row_idx, 5, solar_yield)
+      summary_worksheet.write(row_idx, 6, grid_yield)
+      summary_worksheet.write(row_idx, 7, genset_yield)
+      summary_worksheet.write(row_idx, 8, total_month_savings)
 
-    summary_worksheet.write(len(self.months_list) + 1, 0, "Total")
+      total_kwh_saved_year += total_kwh_saved
+      total_num_outages_year += num_outages
+      total_genset_fuel_savings_year += genset_fuel_savings
+      total_outage_savings_year += outage_savings
+      total_solar_yield_year += solar_yield
+      total_grid_yield_year += grid_yield
+      total_genset_yield_year += genset_yield
+      total_savings_year += total_month_savings
+
+    summary_worksheet.write(len(self.months_list) + 1, 0, "Yearly Totals")
     summary_worksheet.write(len(self.months_list) + 1, 1, total_kwh_saved_year)
     summary_worksheet.write(len(self.months_list) + 1, 2, total_num_outages_year)
     summary_worksheet.write(len(self.months_list) + 1, 3, total_genset_fuel_savings_year)
     summary_worksheet.write(len(self.months_list) + 1, 4, total_outage_savings_year)
+    summary_worksheet.write(len(self.months_list) + 1, 5, total_solar_yield_year)
+    summary_worksheet.write(len(self.months_list) + 1, 6, total_grid_yield_year)
+    summary_worksheet.write(len(self.months_list) + 1, 7, total_genset_yield_year)
+    summary_worksheet.write(len(self.months_list) + 1, 8, total_savings_year)
 
     logging.info(f"Completed calculating yearly savings and writing to summary worksheet.")
 
